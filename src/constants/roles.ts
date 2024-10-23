34dbe5c6621f9e3e6bd22ee7df2ede691bdb8f05
reuelsilva <reuelsilva19@gmail.com>
2024-10-23 19:10:44 -0300

    chore: adiciona uma constante ROLES
    - concentra o array de cargos em uma constante ROLES para otimizar o controle e a reutilização das opções de cargo do formulário

---
+++ src/constants/roles.ts
@@ -0,0 +1,17 @@
+export const ROLES:string[] = [
+    "Desenvolvedor Frontend", 
+    "Desenvolvedor Backend", 
+    "Desenvolvedor Full Stack", 
+    "Desenvolvedor Mobile", 
+    "Desenvolvedor de Software", 
+    "Engenheiro de Software", 
+    "Arquiteto de Software", 
+    "UI/UX Designer", 
+    "Analista de Sistemas", 
+    "Analista Programador", 
+    "DevOps Engineer", 
+    "Engenheiro de Dados", 
+    "QA Engineer", 
+    "Scrum Master", 
+    "Product Owner"
+]
