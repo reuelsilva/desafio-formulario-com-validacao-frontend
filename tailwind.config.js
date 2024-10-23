992487777c3fbd0f55d78372aeb77092bdb937dc
reuelsilva <reuelsilva19@gmail.com>
2024-10-23 19:04:20 -0300

    style: adiciona extensões de cores da paleta de cor

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-01': '#032443',
+        'color-02': '#0000007d',
+      }
+    },
   },
   plugins: [],
 }
