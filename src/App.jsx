5c89fe7f7718baa85afd70ba80e039c18aa95ccc
reuelsilva <reuelsilva19@gmail.com>
2024-10-23 19:18:49 -0300

    feat: adiciona Form, Footer e o topo do formulário ao App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-
-import './App.css'
+import { useContext } from "react";
+import Form from "./components/Form";
+import { SubmittedContext } from "./contexts/submitted-context";
+import Footer from "./components/Footer";
 
 function App() {
+  const { isSubmitted } = useContext(SubmittedContext)
 
   return (
     <>
-      <main>
-        <h1>Seja bem vindo! :)</h1>
-        <p>Terceiro Desafio: Formulário de Cadastro de Membros</p>
-      </main>
+      <div className="flex flex-col gap-2 max-w-[640px] w-full sm:min-h-[calc(100vh-130px)] mx-auto my-0 p-3">
+        <div className="bg-white border border-solid border-gray-200 rounded-lg overflow-hidden">
+          <div className="bg-color-01 h-4"></div>
+          <div className="p-3">
+            <h1 className="text-xl font-medium uppercase">Projeto Front-end Fusion</h1>
+          </div>
+          {
+            !isSubmitted ? (<div className="border-t border-gray-200 border-solid p-3">
+              <p className="text-red-500">* Indica uma pergunta obrigatória</p>
+            </div>) : (<div className="border-t border-gray-200 border-solid p-3">
+              <p className="text-black">Obrigado pelo seu interesse. <br /> Alguém entrará em contato com você em breve!</p>
+            </div>)
+          }
+        </div>
+        {
+          !isSubmitted && (<Form />)
+        }
+      </div>
+      <Footer/>
     </>
   );
 }
