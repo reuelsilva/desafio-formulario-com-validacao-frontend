0f31dd2a1bcb8071f58335f921fbe7af3e196e18
reuelsilva <reuelsilva19@gmail.com>
2024-10-23 19:17:38 -0300

    feat: adiciona o componente Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer():JSX.Element {
+    return (
+        <footer className="flex justify-center bg-color-01 p-6 mt-[42px]">
+            <p className="text-gray-200 text-xs sm:text-sm text-center max-w-[250px]">Formulário Projeto Frontend Fusion - Desenvolvedor Reuel Silva</p>
+        </footer>
+    )
+}
+
+export default Footer
